51873c02f5f306e88d41815ad3c906009965ed2d
hoolka <michalcarnogursky@gmail.com>
2018-06-19 20:36:55 +0200

    test1 added
    tralala

---
+++ admin.php
@@ -0,0 +1,94 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['email'])) {
+    $_SESSION['msg'] = "You must log in first";
+    header('location: login.php');
+}
+
+if (!($_SESSION['role'] == 'ADM')) {
+    $_SESSION['msg'] = "You Don't Have Permission to Access";
+    header('location: login.php');
+}
+
+if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['email']);
+    header("location: login.php");
+}
+?>
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Admin page</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <div class="header">
+            <h2>Register HOT</h2>
+        </div>
+
+        <form method="post" action="admin.php">
+
+            <?php include('errors.php'); ?>
+
+
+            <div class="input-group">
+                <label>Email</label>
+                <input type="email" name="email" value="<?php echo $email; ?>">
+            </div>
+
+            <div class="input-group">
+                <label>First Name</label>
+                <input type="input" name="first_name">
+            </div>
+
+            <div class="input-group">
+                <label>Last Name</label>
+                <input type="input" name="last_name">
+            </div>
+
+            <div class="input-group">
+                <label>Vacation</label>
+                <input type="input" name="vacation">
+            </div>
+
+            <div class="input-group">
+                <label>Home Office</label>
+                <input type="input" name="ho">
+            </div>
+
+            <div class="input-group">
+                <label>Visit To Doctor</label>
+                <input type="input" name="vtd" >
+            </div>
+
+            <div class="input-group">
+                <label>Visit To Doctor w Family Member</label>
+                <input type="input" name="vtd_fm">
+            </div>
+
+            <div class="input-group">
+                <label>Password</label>
+                <input type="password" name="password_1">
+            </div>
+            <div class="input-group">
+                <label>Confirm password</label>
+                <input type="password" name="password_2">
+            </div>
+            <div class="input-group">
+                <button type="submit" class="btn" name="reg_hot">Register</button>
+            </div>
+
+
+            <?php if (isset($_SESSION['email'])) : ?>
+                <p>Welcome <strong><?php echo $_SESSION['email']; ?></strong></p>
+                <p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
+            <?php endif ?>
+<!--            <p>
+    Already a member? <a href="login.php">Sign in</a>
+</p>-->
+        </form>
+    </body>
+</html>
---
+++ server.php
@@ -0,0 +1,176 @@
+<?php
+
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+// variable declaration
+$username = "";
+$email = "";
+$errors = array();
+$_SESSION['success'] = "";
+
+// connect to database
+$db = mysqli_connect('172.20.10.3', 'root', 'Safepass18!', 'hackathon');
+//$db = mysqli_connect('172.20.10.3', 'root', 'SafePass18!', 'hackathon');
+// REGISTER USER
+if (isset($_POST['reg_user'])) {
+    // receive all input values from the form
+    $email = mysqli_real_escape_string($db, $_POST['email']);
+    $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+    $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+
+    // form validation: ensure that the form is correctly filled
+    if (empty($email)) {
+        array_push($errors, "Email is required");
+    }
+    if (empty($password_1)) {
+        array_push($errors, "Password is required");
+    }
+
+    if ($password_1 != $password_2) {
+        array_push($errors, "The two passwords do not match");
+    }
+
+    // register user if there are no errors in the form
+    if (count($errors) == 0) {
+        $password = md5($password_1); //encrypt the password before saving in the database
+        $query = "INSERT INTO users (email, password, role, first_name, last_name, vacation, sick, ho, vtd, vtd_fm, team) 
+					  VALUES('$email', '$password', '$role', '$first_name', '$last_name', '$vacation', '$ho', '$vtd', '$vtd_fm', '$team')";
+        mysqli_query($db, $query);
+
+        //$_SESSION['username'] = $email;
+        //$_SESSION['success'] = "You are now logged in";
+        header('location: index.php');
+    }
+}
+//Reg head of team
+if (isset($_POST['reg_hot'])) {
+    // receive all input values from the form
+    $email = mysqli_real_escape_string($db, $_POST['email']);
+    $role = 'HOT';
+    $first_name = mysqli_real_escape_string($db, $_POST['first_name']);
+    $last_name = mysqli_real_escape_string($db, $_POST['last_name']);
+    $vacation = mysqli_real_escape_string($db, $_POST['vacation']);
+    $ho = mysqli_real_escape_string($db, $_POST['ho']);
+    $vtd = mysqli_real_escape_string($db, $_POST['vtd']);
+    $vtd_fm = mysqli_real_escape_string($db, $_POST['vtd_fm']);
+    $team = md5(uniqid($_SESSION['email'], true));
+    $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+    $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+
+    // form validation: ensure that the form is correctly filled
+    if (empty($email)) {
+        array_push($errors, "Email is required");
+    }
+    if (empty($password_1)) {
+        array_push($errors, "Password is required");
+    }
+
+    if ($password_1 != $password_2) {
+        array_push($errors, "The two passwords do not match");
+    }
+
+    // register user if there are no errors in the form
+    if (count($errors) == 0) {
+        $password = md5($password_1); //encrypt the password before saving in the database
+        $query = "INSERT INTO users (email, password, role, first_name, last_name, vacation, ho, vtd, vtd_fm, team) 
+					  VALUES('$email', '$password', '$role', '$first_name', '$last_name', '$vacation', '$ho', '$vtd', '$vtd_fm', '$team')";
+        mysqli_query($db, $query);
+
+        //$_SESSION['email'] = $email;
+        //$_SESSION['success'] = "You are now logged in";
+        //header('location: hot.php');
+    }
+}
+
+
+if (isset($_POST['reg_emp'])) {
+    // receive all input values from the form
+    $email = mysqli_real_escape_string($db, $_POST['email']);
+    $role = 'EMP';
+    $first_name = mysqli_real_escape_string($db, $_POST['first_name']);
+    $last_name = mysqli_real_escape_string($db, $_POST['last_name']);
+    $vacation = mysqli_real_escape_string($db, $_POST['vacation']);
+    $ho = mysqli_real_escape_string($db, $_POST['ho']);
+    $vtd = mysqli_real_escape_string($db, $_POST['vtd']);
+    $vtd_fm = mysqli_real_escape_string($db, $_POST['vtd_fm']);
+    $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+    $em = $_SESSION['email'];
+    $query = "SELECT team from users where email = '$em'";
+    mysqli_query($db, $query);
+
+    $team = $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+
+    // form validation: ensure that the form is correctly filled
+    if (empty($email)) {
+        array_push($errors, "Email is required");
+    }
+    if (empty($password_1)) {
+        array_push($errors, "Password is required");
+    }
+
+    if ($password_1 != $password_2) {
+        array_push($errors, "The two passwords do not match");
+    }
+
+    // register user if there are no errors in the form
+    if (count($errors) == 0) {
+        $password = md5($password_1); //encrypt the password before saving in the database
+        $query = "INSERT INTO users (email, password, role, first_name, last_name, vacation, ho, vtd, vtd_fm) 
+					  VALUES('$email', '$password', '$role', '$first_name', '$last_name', '$vacation', '$ho', '$vtd', '$vtd_fm')";
+        mysqli_query($db, $query);
+
+        //$_SESSION['email'] = $email;
+        //$_SESSION['success'] = "You are now logged in";
+        //header('location: hot.php');
+    }
+}
+
+// ... 
+// LOGIN USER
+if (isset($_POST['login_user'])) {
+    $email = mysqli_real_escape_string($db, $_POST['email']);
+    $password = mysqli_real_escape_string($db, $_POST['password']);
+    $role = mysqli_real_escape_string($db, $_POST['password']);
+
+
+    if (empty($email)) {
+        array_push($errors, "Email is required");
+    }
+    if (empty($password)) {
+        array_push($errors, "Password is required");
+    }
+
+    if (count($errors) == 0) {
+        $password = md5($password);
+        $query = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+        $results = mysqli_query($db, $query);
+        
+        $rolequery = "SELECT role FROM users WHERE email='$email' AND password='$password'";
+        $roleresult = mysqli_query($db, $rolequery);
+
+        $_SESSION['role'] = $roleresult;
+        
+       
+        if (mysqli_num_rows($results) == 1) {
+            if ($_SESSION['role'] == 'ADM') {
+                $_SESSION['email'] = $email;
+                $_SESSION['success'] = "You are now logged in";
+                header('location: admin.php');
+            } else if ($_SESSION['role'] == 'HOT') {
+                $_SESSION['email'] = $email;
+                $_SESSION['success'] = "You are now logged in";
+                header('location: hot.php');
+            } else {
+
+                $_SESSION['email'] = $email;
+                $_SESSION['success'] = "You are now logged in";
+                header('location: hot.php');
+            }
+        } else {
+            array_push($errors, "Wrong username/password combination");
+        }
+    }
+}
+?>
